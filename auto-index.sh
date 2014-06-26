f0fa40fa61f3cfd81db3989e2912d67ff42ac0e8
Jeroen <jeroenooms@gmail.com>
2014-06-25 21:06:27 -0700

    add index.html files

---
+++ auto-index.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+# usage: auto-index [dir]
+INDEX=`ls -1 $1 | sed "s/^.*/      <li\>\<a\ href=\"&\"\>&\<\\/a\>\<\\/li\>/"`
+echo "<html>
+  <head><title>Index of $1</title></head>
+  <body>
+    <h2>Index of $1</h2>
+    <hr>
+    <ul>
+$INDEX
+    <ul>
+  </body>
+</html>"
+
